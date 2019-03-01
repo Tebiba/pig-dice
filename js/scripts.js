fa99a5ceb0bd2a49cec11c26f2722c024bdcc7a1
Tebiba <Tebiba@users.noreply.github.com>
2019-03-01 15:35:06 +0300

    Created well and columns for Player 1 and Player 2, changed the color of the buttons

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// business logic
+
+function rollDice(){
+    var die1 = document.getElementById("die1");
+    var die2 = document.getElementById("die2");
+    var status = document.getElementById("status");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d1 + d2;
+    die1.innerHTML = d1;
+    die2.innerHTML = d2;
+    status.innerHTML = diceTotal;
+}
+if(d1 == d2){
+    status.innerHTML += " DOUBLES! You get a free turn!!";
+}
+
+/*function rollDice(){
+    var die1 = document.getElementById("die3");
+    var die2 = document.getElementById("die4");
+    var status = document.getElementById("status");
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = d3 + d4;
+    die1.innerHTML = d3;
+    die2.innerHTML = d4;
+    status.innerHTML = diceTotal;
+}*/
